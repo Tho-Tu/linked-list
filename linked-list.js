4c2d0555ab6b68468f125c6e2f13af141a7518ff
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-10-12 21:26:56 +1000

    Complete linked list

--- linked-list.js
+++ linked-list.js
@@ -1,18 +1,23 @@
+let ll = linkedList();
+
 function linkedList() {
+  const head = (() => {
+    return node("head");
+  })();
+
   const append = (value) => {
-    let newNode = node(value, null);
-    tail.nextNode = newNode;
+    tail().nextNode = node(value, null);
   };
 
   const prepend = (value) => {
-    let newNode = node(value, head().nextNode);
-    head().nextNode = newNode;
+    let newNode = node(value, head.nextNode);
+    head.nextNode = newNode;
   };
 
   const size = () => {
-    let sum = 0;
+    let sum = 1;
 
-    traverseLinkedList(head());
+    traverseLinkedList(head);
 
     function traverseLinkedList(node) {
       if (node.nextNode !== null) {
@@ -24,62 +29,103 @@ function linkedList() {
     return sum;
   };
 
-  const head = () => {
-    let headNode = node();
-    return headNode;
-  };
-
   const tail = () => {
-    traverseLinkedList(head());
     function traverseLinkedList(node) {
       if (node.nextNode === null) {
-        node.nextNode;
+        return node;
       } else {
-        traverseLinkedList(node.nextNode);
+        return traverseLinkedList(node.nextNode);
       }
     }
+
+    return traverseLinkedList(head);
   };
 
   const at = (index) => {
     let checkIndex = 0;
 
-    traverseLinkedList(head());
     function traverseLinkedList(node) {
       if (checkIndex === index) {
         return node;
       } else {
         checkIndex += 1;
-        traverseLinkedList(node.nextNode);
+        return traverseLinkedList(node.nextNode);
       }
     }
+
+    return traverseLinkedList(head);
   };
 
   const pop = () => {
-    traverseLinkedList(head());
+    if (head.nextNode === null) {
+      // Linked list is empty, nothing to pop
+      return null;
+    }
+
+    if (head.nextNode.nextNode === null) {
+      // Linked list has only one element
+      const poppedValue = head.nextNode.value;
+      head.nextNode = null;
+      return poppedValue;
+    }
+
     function traverseLinkedList(node) {
       if (node.nextNode.nextNode === null) {
+        const poppedValue = node.nextNode.value;
         node.nextNode = null;
+        return poppedValue;
+      } else {
+        return traverseLinkedList(node.nextNode);
       }
     }
+
+    return traverseLinkedList(head);
   };
 
   const contains = (value) => {
-    traverseLinkedList(head());
     function traverseLinkedList(node) {
       if (node.value === value) {
         return true;
+      } else if (node.nextNode === null) {
+        return false;
       } else {
-        traverseLinkedList(node.nextNode);
+        return traverseLinkedList(node.nextNode);
       }
     }
+
+    return traverseLinkedList(head);
   };
 
   const find = (value) => {
-    //
+    let index = 0;
+
+    function traverseLinkedList(node) {
+      if (node.value === value) {
+        return index;
+      } else if (node.nextNode === null) {
+        return null;
+      } else {
+        index += 1;
+        return traverseLinkedList(node.nextNode);
+      }
+    }
+
+    return traverseLinkedList(head);
   };
 
   const toString = () => {
-    //
+    let linkedListString = "";
+    function traverseLinkedList(node) {
+      if (node.nextNode === null) {
+        linkedListString += `( ${node.value} ) -> null`;
+      } else {
+        linkedListString += `( ${node.value} ) -> `;
+        traverseLinkedList(node.nextNode);
+      }
+    }
+    traverseLinkedList(head);
+
+    console.log(linkedListString);
   };
 
   return {
